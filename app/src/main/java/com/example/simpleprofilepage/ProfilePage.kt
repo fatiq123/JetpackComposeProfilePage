54a52fd1150bcec494a3fbdf92d17ffe41d29da2
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-09 23:07:43 -0700

    Constraint layout and Other Basic Components

---
+++ app/src/main/java/com/example/simpleprofilepage/ProfilePage.kt
@@ -0,0 +1,188 @@
+package com.example.simpleprofilepage
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+
+@Composable
+fun ProfilePage() {
+
+    // using the constraint layout
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp, bottom = 100.dp, start = 16.dp, end = 16.dp)
+            .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(15.dp))
+    ) {
+        ConstraintLayout {
+
+            val image = createRef()
+            val nameText = createRef()
+            val placeText = createRef()
+            val rowStat = createRef()
+            val followButton = createRef()
+            val unfollowButton = createRef()
+
+            val guideLine = createGuidelineFromTop(0.1f)
+
+            Image(
+                painter = painterResource(id = R.drawable.mypic),
+                contentDescription = "My Picture",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(shape = CircleShape)
+                    .border(width = 2.dp, color = Color.Red, shape = CircleShape)
+                    .constrainAs(image) {
+                        top.linkTo(guideLine)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "Fatiq Hussnain",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.constrainAs(nameText) {
+                    top.linkTo(image.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
+            Text(text = "ISSB", modifier = Modifier.constrainAs(placeText) {
+                top.linkTo(nameText.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .constrainAs(rowStat) {
+                        top.linkTo(placeText.bottom)
+                    }
+            ) {
+                ProfileStats(count = "150", title = "Followers")
+                ProfileStats(count = "100", title = "Following")
+                ProfileStats(count = "10", title = "Posts")
+            }
+
+            Button(onClick = { }, modifier = Modifier.constrainAs(followButton) {
+                top.linkTo(rowStat.bottom, margin = 16.dp)
+                start.linkTo(parent.start)
+                end.linkTo(unfollowButton.start)
+                width = Dimension.wrapContent
+            }) {
+                Text(text = "Follow")
+            }
+            Button(onClick = { }, modifier = Modifier.constrainAs(unfollowButton) {
+                top.linkTo(rowStat.bottom, margin = 16.dp)
+                start.linkTo(followButton.end)
+                end.linkTo(parent.end)
+                width = Dimension.wrapContent
+            }) {
+                Text(text = "UnFollow")
+            }
+
+        }
+    }
+
+
+    /*Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp, bottom = 100.dp, start = 16.dp, end = 16.dp)
+            .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(15.dp))
+    ) {
+        Column(
+            Modifier.verticalScroll(rememberScrollState()).padding(30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.mypic),
+                contentDescription = "My Picture",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(shape = CircleShape)
+                    .border(width = 2.dp, color = Color.Red, shape = CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(text = "Fatiq Hussnain", fontWeight = FontWeight.Bold)
+            Text(text = "ISSB")
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ProfileStats(count = "150", title = "Followers")
+                ProfileStats(count = "100", title = "Following")
+                ProfileStats(count = "10", title = "Posts")
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Button(onClick = { }) {
+                    Text(text = "Follow")
+                }
+                Button(onClick = { }) {
+                    Text(text = "UnFollow")
+                }
+            }
+        }
+    }*/
+
+}
+
+@Composable
+fun ProfileStats(count: String, title: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = count, fontWeight = FontWeight.Bold)
+        Text(text = title)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfilePagePreview() {
+    ProfilePage()
+}
